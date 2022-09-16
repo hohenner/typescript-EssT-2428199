b0dda35987536f30eb4a90faa0fc628462c1ad0c
Andy Hohenner <ahohenner@mdsol.com>
2022-09-16 14:27:29 -0700

    exercise 03_01

---
+++ exercise_files/Ch03/03_01_andy/src/demo.ts
@@ -0,0 +1,45 @@
+type ContactName = string;
+
+// enum ContactStatus {
+//     Active = "active",
+//     Inactive = "inactive",
+//     New = "new"
+// }
+type ContactStatus = "active" | "inactive" | "new"
+type ContactBirthDate = Date | number | string
+
+interface Contact {
+    id: number;
+    name: ContactName;
+    birthDate?: ContactBirthDate;
+    status?: ContactStatus;
+}
+
+interface Address {
+    line1: string;
+    line2: string;
+    province: string;
+    region: string;
+    postalCode: string;
+}
+
+// interface AddressableContact extends Address, Contact {}
+type AddressableContact = Address & Contact
+
+function getBirthDate(contact: Contact) {
+    if (typeof contact.birthDate === "number") {
+        return new Date(contact.birthDate);
+    }
+    else if (typeof contact.birthDate === "string") {
+        return Date.parse(contact.birthDate)
+    }
+    else {
+        return contact.birthDate
+    }
+}
+
+let primaryContact: Contact = {
+    id: 12345,
+    name: "Jamie Johnson",
+    status: "active"
+}
